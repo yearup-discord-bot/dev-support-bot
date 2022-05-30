84f7e5a3ec60acb95e23a33ce08338b73965e2aa
carloscruzyearup <carlos.cruz3@seattlecolleges.edu>
2022-05-29 17:33:20 -0700

    Add server side http logging

--- events/ready.js
+++ events/ready.js
@@ -10,29 +10,30 @@ module.exports = {
 	// the following function is what will be executed once our bot is notified of this event occurring
 	execute(client) {
 
-		const Tail = require('tail-file');
-		const fs = require('node:fs');
-
-		const {execSync} = require('child_process');
-		// get the date to use as the name for the current days log
-		// need to cut out the last trailing newline character to avoid mucking up the file name
-		const stdout = execSync('date +%m-%d-%y | tr -d "\n"');
-
 		// get a handle to the statically defined channel in config for the dev console output
-		client.debugLogChannel = client.channels.cache.get(client.debugLogChannelId);
+		const { debugChannelID } = require('../config.json');
+		client.debugChannel = client.channels.cache.get( debugChannelID );
 
-		if ( client.debugLogChannel )
+		if ( client.debugChannel )
 		{
-		if ( !fs.existsSync(`../logs/${stdout}.log`) )
-			fs.writeFile( `../logs/${stdout}.log`, '---- BEGIN LOG ----\n', () => {} )
-	
-			// the default folder for logs will be in the up one level in a folder call 'logs'
-			client.debugLogTail = new Tail(`../logs/${stdout}.log`, { force: true }, line => {
-				// each time a new line is read the content is sent to the dev log channel
-				// IF there is actual data to send otherwise discord.js craps out
-				// crying about not being able to send an empty string :'(
-				if ( line.length > 0 )
-					client.debugLogChannel.send( line );
+			const http = require('node:http');	
+			const hostname = '127.0.0.1';
+			const port = '5656';
+
+			const server = http.createServer((req, res) => {
+				let body = '';
+
+				req.on('data', chunk => {
+					body += chunk.toString(); // convert Buffer to string
+				});
+
+				req.on('end', () => {
+					client.debugChannel.send(body);
+				});
+			});
+
+			server.listen(port, hostname, () =>{
+				console.log(`Listening for log messages at http://${hostname}:${port}`);
 			});
 		}
 
